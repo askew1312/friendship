d29430e948a448572a02c33723438f404a58b2bc
eli <e.feasley@gmail.com>
2020-03-17 12:13:41 -0400

    Updates

---
+++ precache-manifest.70d30662f3c721f1dfc2636d7266ffc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f48a01b100581a58caf4da9352f4d81",
+    "url": "/friendship/index.html"
+  },
+  {
+    "revision": "9c4ac6af36fbad971f37",
+    "url": "/friendship/static/css/main.39c09b30.chunk.css"
+  },
+  {
+    "revision": "a1e2a6019379c38a345a",
+    "url": "/friendship/static/js/2.4ef3896a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/friendship/static/js/2.4ef3896a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4ac6af36fbad971f37",
+    "url": "/friendship/static/js/main.35af85af.chunk.js"
+  },
+  {
+    "revision": "c6ef6ffb509b64735b20",
+    "url": "/friendship/static/js/runtime-main.c7241158.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/friendship/static/media/logo.5d5d9eef.svg"
+  }
+]);
